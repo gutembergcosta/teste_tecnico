335333e36125c802d29068cc60bf24de75c05357
gutemberg <gutembergcosta01@gmail.com>
2021-04-07 15:05:43 -0300

    Atualização script de banco

--- DB_script.sql
+++ DB_script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05-Abr-2021 às 06:38
+-- Tempo de geração: 07-Abr-2021 às 20:02
 -- Versão do servidor: 10.4.14-MariaDB
 -- versão do PHP: 7.4.9
 
@@ -20,104 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `bernoulli`
 --
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `categorias`
---
-
-CREATE TABLE `categorias` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `categorias`
---
-
-INSERT INTO `categorias` (`id`, `nome`, `created_at`, `updated_at`) VALUES
-(1, 'Categoria 01', NULL, NULL),
-(2, 'Categoria 02', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `item`
---
-
-CREATE TABLE `item` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL,
-  `tipo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `item`
---
-
-INSERT INTO `item` (`id`, `nome`, `id_user`, `id_categoria`, `tipo`, `created_at`, `updated_at`) VALUES
-(3, 'Nome 03', 0, 2, 'teste', NULL, '2021-04-04 15:04:07'),
-(5, 'teste 031', 0, 2, 'teste', '2021-03-20 16:35:13', '2021-04-04 15:09:15'),
-(8, 'asdf', 0, 2, 'teste', '2021-03-22 00:11:44', '2021-03-22 00:11:44');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2021_02_28_122240_create_model_items_table', 2),
-(5, '2021_02_28_122402_create_model_categorias_table', 2),
-(7, '2021_02_28_130653_add_tipo_to_item_table', 3),
-(8, '2021_04_04_174007_create_model_times_table', 4);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -144,67 +46,29 @@ CREATE TABLE `times` (
 --
 
 INSERT INTO `times` (`id`, `nome`, `pontos`, `jogos`, `vitorias`, `empates`, `derrotas`, `gols_feitos`, `gols_sofridos`, `created_at`, `updated_at`) VALUES
-(1, 'América Mineiro', 14, 9, 3, 5, 1, 8, 4, '2021-04-03 03:28:54', '2021-04-05 07:07:53'),
-(2, 'Athletico Paranaense', 4, 5, 0, 4, 1, 1, 4, '2021-04-03 03:28:50', '2021-04-05 07:07:53'),
-(3, 'Atlético Goianiense', 4, 4, 1, 1, 2, 6, 7, '2021-04-03 03:28:37', '2021-04-05 06:58:20');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'gutembergcosta01@gmail.com', NULL, '$2y$10$xv7bpdSbNDbam3rCYzGiEO5OWj5MMoFIIrQqlDDJY1IJZJc9aHZ0y', NULL, '2021-03-21 02:48:42', '2021-03-21 02:48:42');
+(1, 'América Mineiro - MG', 36, 24, 8, 12, 4, 31, 16, '2021-04-03 03:28:54', '2021-04-05 19:59:06'),
+(2, 'Athletico Paranaense - PR', 14, 11, 2, 8, 1, 2, 8, '2021-04-03 03:28:50', '2021-04-05 20:01:16'),
+(3, 'Atlético Goianiense - GO', 19, 15, 5, 4, 6, 23, 34, '2021-04-03 03:28:37', '2021-04-05 20:04:27'),
+(4, 'Atlético Mineiro - MG', 16, 9, 5, 1, 3, 23, 25, NULL, '2021-04-05 20:00:36'),
+(5, 'Bahia - BA', 21, 18, 4, 9, 5, 21, 31, NULL, '2021-04-05 20:04:27'),
+(6, 'Chapecoense	- SC', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(7, 'Corinthians	- SP', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(10, 'Cuiabá - MT', 1, 1, 0, 1, 0, 0, 0, NULL, '2021-04-05 20:01:16'),
+(12, 'Flamengo - RJ', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(13, 'Fluminense - RJ', 3, 1, 1, 0, 0, 1, 0, NULL, '2021-04-05 13:43:47'),
+(14, 'Fortaleza - CE', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(15, 'Grêmio- RS', 5, 3, 1, 2, 0, 1, 0, NULL, '2021-04-05 08:51:34'),
+(16, 'Internacional - RS', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(17, 'Juventude - RS', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(18, 'Palmeiras - SP', 0, 0, 0, 0, 0, 0, 0, NULL, NULL),
+(19, 'Red Bull - SP', 3, 1, 1, 0, 0, 3, 0, NULL, '2021-04-05 14:08:57'),
+(20, 'Santos - SP', 11, 11, 3, 2, 6, 17, 23, NULL, '2021-04-05 14:01:51'),
+(21, 'São Paulo - SP', 4, 2, 1, 1, 0, 4, 3, NULL, '2021-04-05 13:55:29'),
+(22, 'Sport - PE', 12, 8, 4, 0, 4, 18, 16, NULL, '2021-04-05 19:57:35');
 
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices para tabela `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Índices para tabela `times`
@@ -213,51 +77,14 @@ ALTER TABLE `times`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
 -- AUTO_INCREMENT de tabelas despejadas
 --
-
---
--- AUTO_INCREMENT de tabela `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de tabela `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT de tabela `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `times`
 --
 ALTER TABLE `times`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
